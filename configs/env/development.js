36c8f46b1e83490d7ab0c96319d9056fb536e3bc
soniharshil54 <sharshil43@yahoo.com>
2021-01-10 18:08:51 +0530

    cr: change db run in dev env

--- configs/env/development.js
+++ configs/env/development.js
@@ -2,7 +2,7 @@ module.exports = {
     host     : 'localhost',
     user     : 'soniuser',
     password : 'soni3360',
-    database : 'testdb',
+    database : 'filesystem',
     db_port: 3306,
     server_port: 5600
 }
